132d6a3df1650292f3e26e112de6ebec4e9fe355
Marc <marcph94@gmail.com>
2022-04-19 16:52:30 +0200

    primera parte

--- WhoPay/app/src/main/java/com/marc/whopay/MainActivity.java
+++ WhoPay/app/src/main/java/com/marc/whopay/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MultitouchView myTouchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        myTouchView= new MultitouchView(this);
+        setContentView(myTouchView);
     }
 }
